10887388d6ac39faf40ba4ee14e5f7c06a04e0a1
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-03-19 10:09:55 +1100

    19/03
    I worked on my user interface today, and have established majority of the core user input

--- main.py
+++ main.py
@@ -1,12 +1,86 @@
 def main():
-    userinput = ""
+
+    userinput = "" 
+    pokemonName = ""
+    moveName = ""
+    typeOfData = ""
+    
+
     while userinput != "end":
         userinput = input("").lower()
         if userinput.startswith("search "):
-            userinput = userinput.replace("search ", "")
-            print("here's that pokemon's details!")
+            pokemonName = userinput.replace("search ", "") #removes the key term used so that the name of the pokemon can be found by the api
+            print(Search_Pokemon(pokemonName))
+
+
+        elif userinput.startswith("store "):
+            pokemonName = userinput.replace("store ", "") #removes the key term used so that the name of the pokemon can be found by the api
+            print(Store_Pokemon(pokemonName))
+        
+        elif userinput.startswith("check "):
+            pokemonName = userinput.replace("check ", "") #removes the key term used so that the name of the pokemon can be found by the api
+            print(Check_Pokemon(pokemonName))
+        
+        elif userinput == "view team":
+            print(View_Team())
+
+        elif userinput.startswith("remove "):
+            pokemonName = userinput.replace("remove ", "") #removes the key term used so that the name of the pokemon can be found by the api
+            print(Remove_Pokemon(pokemonName))
+        
+        elif userinput.startswith("give "):
+            pokemonName = userinput.replace("give ", "")
+
+        elif userinput == "end":
+            print("goodbye")
+
         else:
             print("invalid input")
+
+def Search_Pokemon(pokemon):
+    return "here's" + pokemon + "'s details!"
+
+def Store_Pokemon(pokemon):
+    return pokemon + "has been added to party!"
+
+def Check_Pokemon(pokemon):
+
+    if " level up moveset" in pokemon:
+        pokemon = pokemon.replace(" level up moveset", "")
+        return "this is the level up moveset of " + pokemon
+    
+    elif " evolution line" in pokemon:
+        pokemon = pokemon.replace(" evolution line", "")
+        return "this is the evolution line of " + pokemon
+    
+    elif " bst distribution" in pokemon:
+        pokemon = pokemon.replace(" bst distribution", "")
+        return "this is the base stat total distribution of " + pokemon
+    
+    elif " type" in pokemon:
+        pokemon = pokemon.replace(" type", "")
+        return "this is the type of " + pokemon
+
+    #determines which kind of data the user wants to retrieve from the database
+    
+    else:
+        return "invalid input"
+
+
+def View_Team():
+    return "This is your team"
+
+def Remove_Pokemon(pokemon):
+    return pokemon + "has been removed from party!"
+
+#$def Give_Move():
+
+#def Challenge():
+
+#def help():
+
+
+
 
 print("Hello there! Welcome to the world of pokémon! This world is inhabited by creatures called pokémon! For some people, pokémon are pets. Others use them for fights.")
 print("If you require instructions try typing 'help' ")
